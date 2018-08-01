0ab5229c3686eb156175ee470ca286eecaeacbd5
bbaayezi@woobest <zhour@woobest.com>
2018-08-01 18:24:52 +0800

    start writing standards

---
+++ Scripts/Interface/ISpawnable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Project.Standard.Interface
+{
+	// This interface is used to limit the function of bullet spawner
+	public interface ISpawnable
+	{
+		void SpawnBullet();
+	}
+}
